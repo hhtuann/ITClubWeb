1db633821636720826aefd2d4b695a2c9419820b
hhtuann <hhtuann.19@gmail.com>
2025-02-15 16:09:19 +0700

    add files

--- src/main.js
+++ src/main.js
@@ -3,18 +3,10 @@ const faqItems = document.querySelectorAll(".faq-item");
 faqItems.forEach((item) => {
   const question = item.querySelector(".faq-question");
   const answer = item.querySelector(".faq-answer");
-  let isHidden = (answer.style.maxHeight === "0");
-  question.addEventListener("click", (e) => {
+
+  question.addEventListener("click", () => {
+    answer.classList.toggle("open");
     const arrow = question.querySelector(".arrow");
-    if(isHidden) {
-      answer.style.maxHeight = "1000px"
-      arrow.textContent = "▲";
-      isHidden = false;
-    }
-    else {
-      answer.style.maxHeight = "0"
-      arrow.textContent = "▼";
-      isHidden = true;
-    }
+    arrow.textContent = answer.classList.contains("open") ? "▲" : "▼";
   });
 });
